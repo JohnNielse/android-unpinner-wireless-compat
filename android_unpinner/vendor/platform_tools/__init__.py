0c1edd826096459c0b12cec1e6d35956345735b4
John Nielsen <56798@vucv.dk>
2023-11-25 10:54:49 +0100

    Update __init__.py
    Choose first devices in adb devices

--- android_unpinner/vendor/platform_tools/__init__.py
+++ android_unpinner/vendor/platform_tools/__init__.py
@@ -15,7 +15,17 @@ else:
 
 def adb(cmd: str) -> subprocess.CompletedProcess[str]:
     """Helper function to call adb and capture stdout."""
-    cmd = f"{adb_binary} {cmd}"
+    # Get a list of all connected devices
+    result = subprocess.run(['adb', 'devices'], stdout=subprocess.PIPE, text=True)
+    devices = result.stdout.splitlines()[1:-1]  # Skip first and last line
+
+    # Extract the serial numbers from the list
+    serial_numbers = [line.split('\t')[0] for line in devices]
+
+    # Choose a device to perform the operations on
+    target_device = serial_numbers[0]
+
+    cmd = f"{adb_binary} -s {target_device} {cmd}"
     try:
         proc = subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
     except subprocess.CalledProcessError as e:
